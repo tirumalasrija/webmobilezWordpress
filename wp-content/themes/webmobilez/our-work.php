b386d0348ebc5a453317d694eaee9eaf5e5d977c
venkatesh <pvenkateshnaidu@gmail.com>
2019-07-24 00:07:19 +0530

    portfolio

--- wp-content/themes/webmobilez/our-work.php
+++ wp-content/themes/webmobilez/our-work.php
@@ -5,7 +5,7 @@
     */
   get_header();
   ?>
-  <section class="breadcrumb_area">
+         <section class="breadcrumb_area">
             <img class="breadcrumb_shap" src="img/breadcrumb/banner_bg.png" alt="">
             <div class="container">
                 <div class="breadcrumb_content text-center">
@@ -14,13 +14,83 @@
                 </div>
             </div>
         </section>
-        <section class="portfolio_area sec_pad bg_color">
+<section class="portfolio_area sec_pad bg_color">
             <div class="container">
-        Working progress
-           
-                </div>
+                <section class="section section--portfolio">
+                        <div class="section__left">
+                            <div class="frame">
+                                <div class="frame__head"> <span class="frame__dots"></span> <span class="addressbar"><a rel="nofollow" href="https://webmobilez.com/" target="_blank">http://WebMobileZ.com/</a></span> <span class="frame__lines"></span> </div>
+                                <div class="frame__body">
+                                    <div class="screenshot" style="background-image:url(<?php echo get_template_directory_uri(); ?>/images/Screenshot.png);"></div>
+                                </div>
+                                <div class="scroll-hint">Hover to scroll</div>
+                            </div>
+                        </div>
+                        <div class="section__right">
+                            <h3 class="section__title" data-text="04">
+                                <span>WebMobileZ</span>
+                                <span class="caption">Powered by WebMobileZ</span>
+                            </h3>
+                            <div class="section__description">
+                                <div class="scrollbar">
+                                    <p>WebMobileZ is an online marketplace that connects travelers directly with local businesses on island destinations in Asia. WebMobileZ delivers the best travel experience by enabling the traveler to choose his experience type such as Culinary, Adventure, Fitness, Water Sports, and more. The company's aim is to raise environmental awareness and social responsibility, which is why a small portion of the fee is automatically donated to charities.<br>
+                                        <br> When WebMobileZ came to FATbit with their idea, they wanted to stand apart from the rest of the travel portals. By analyzing their targeted user base our designers formulated a website design, which scored very high on user experience. Rather than going for a traditional website, we tried a new approach of offering all the crucial information on the homepage itself and carefully segregating everything into different sections. This made the whole website user engaging.</p>
+                                </div>
+                            </div>
+                            <div class="section_client-detail">
+                                <ul class="list--vertical">
+                                    <li> <span class="list__title">Client</span> <span class="list__subtitle">WebMobileZ</span> </li>
+                                    <li> <span class="list__title">technology</span> <span class="list__subtitle">HTML5 &amp; CSS3, JS, Responsive, Custom</span> </li>
+
+                                </ul>
+                                <div class="btns-wrapper"><a href="#" class="btn btn--primary">Request a FREE Quote</a>
+                                    <a rel="nofollow" href="https://webmobilez.com/" target="_blank" class="link btn btn--border btn--border-primary">Visit Website</a>
+                                </div>
+                            </div>
+                        </div>
+                </section>
+
+                <!-- Ssection 2 -->
+
+                <section class="section section--portfolio">
+                        <div class="section__left">
+                            <div class="frame">
+                                <div class="frame__head"> <span class="frame__dots"></span> <span class="addressbar"><a rel="nofollow" href="https://webmobilez.com/" target="_blank">http://WebMobileZ.com/</a></span> <span class="frame__lines"></span> </div>
+                                <div class="frame__body">
+                                    <div class="screenshot" style="background-image:url(<?php echo get_template_directory_uri(); ?>/images/Screenshot.png);"></div>
+                                </div>
+                                <div class="scroll-hint">Hover to scroll</div>
+                            </div>
+                        </div>
+                        <div class="section__right">
+                            <h3 class="section__title" data-text="04">
+                                <span>WebMobileZ</span>
+                                <span class="caption">Powered by WebMobileZ</span>
+                            </h3>
+                            <div class="section__description">
+                                <div class="scrollbar">
+                                    <p>WebMobileZ is an online marketplace that connects travelers directly with local businesses on island destinations in Asia. WebMobileZ delivers the best travel experience by enabling the traveler to choose his experience type such as Culinary, Adventure, Fitness, Water Sports, and more. The company's aim is to raise environmental awareness and social responsibility, which is why a small portion of the fee is automatically donated to charities.<br>
+                                        <br> When WebMobileZ came to FATbit with their idea, they wanted to stand apart from the rest of the travel portals. By analyzing their targeted user base our designers formulated a website design, which scored very high on user experience. Rather than going for a traditional website, we tried a new approach of offering all the crucial information on the homepage itself and carefully segregating everything into different sections. This made the whole website user engaging.</p>
+                                </div>
+                            </div>
+                            <div class="section_client-detail">
+                                <ul class="list--vertical">
+                                    <li> <span class="list__title">Client</span> <span class="list__subtitle">WebMobileZ</span> </li>
+                                    <li> <span class="list__title">technology</span> <span class="list__subtitle">HTML5 &amp; CSS3, JS, Responsive, Custom</span> </li>
+
+                                </ul>
+                                <div class="btns-wrapper"><a href="#" class="btn btn--primary">Request a FREE Quote</a>
+                                    <a rel="nofollow" href="https://webmobilez.com/" target="_blank" class="link btn btn--border btn--border-primary">Visit Website</a>
+                                </div>
+                            </div>
+                        </div>
+                </section>
+
+<!-- Ssection 2 -->
+
             </div>
         </section>
+     
 
         <?php 
   
